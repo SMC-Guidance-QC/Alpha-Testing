f1f243bbd962b8a666a28ae271fe24575fd8259e
SMC-Guidance-QC <smcguidanceeval@gmail.com>
2026-09-23 12:05:23 +0800

    Delete SMC-Alpha-test directory

--- SMC-Alpha-test/js/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-window.SMC = window.SMC || {};
-SMC.config = {
-    apiUrl: 'https://script.google.com/macros/s/AKfycbzCv5D5j7c-wOuGHoFHv7SH4m8VkJxATsTk9_QOLlTKiNiuSKNkLF48PRkhxXm4_U6w/exec',
-    perPage: 12,
-    palette: ['#002B6B', '#BFA050', '#C94040', '#2A9D8F', '#D47B2A', '#7859CF', '#3A86C8'],
-    warnBefore: 10 * 60 * 1000,
-    commandCenter: { passphrase: 'command', combo: { ctrl: true, alt: true, key: 'k' } }
-};
--- SMC-Alpha-test/js/theme-init.js
+++
@@ -1,1 +0,0 @@
-"use strict";try{if(localStorage.getItem("smc-theme")==="dark")document.documentElement.setAttribute("data-theme","dark");}catch(e){}document.addEventListener("DOMContentLoaded",function(){var y=document.getElementById("sfYear");if(y)y.textContent=new Date().getFullYear();});
